d6883ad85d04324e8e25df95df456257b9fc6f47
Ayutac <fly.high.android@gmail.com>
2026-08-22 15:25:18 +0200

    Added a removal note.

--- common/src/main/java/studio/abos/mc/strangeadventures/client/ModKeyMappings.java
+++ common/src/main/java/studio/abos/mc/strangeadventures/client/ModKeyMappings.java
@@ -10,10 +10,11 @@ public class ModKeyMappings {
 
     public static ManagedKeyMapping yourKey;
 
+    // TODO remove this before release
     public static void initialize() {
         yourKey = Kuma.createKeyMapping(StrangeAdventures.id("your_key"))
                 .withDefault(InputBinding.key(InputConstants.KEY_B))
-                .handleScreenInput(event -> {
+                .handleScreenInput(_ -> {
                     StrangeAdventures.logger.info("B was pressed - " + StrangeAdventures.MOD_ID);
                     return true;
                 })
