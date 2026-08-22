28c1f293be6beff801f8cb44bc131e38add303d0
Ayutac <fly.high.android@gmail.com>
2026-08-22 21:39:25 +0200

    Added a tank to the sap sipper.

--- common/src/main/java/studio/abos/mc/strangeadventures/blockentity/SapSipperBlockEntity.java
+++ common/src/main/java/studio/abos/mc/strangeadventures/blockentity/SapSipperBlockEntity.java
@@ -1,20 +1,67 @@
 package studio.abos.mc.strangeadventures.blockentity;
 
+import net.blay09.mods.balm.platform.fluid.BalmFluidTankProvider;
+import net.blay09.mods.balm.platform.fluid.DefaultFluidTank;
+import net.blay09.mods.balm.platform.fluid.FluidTank;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
+import studio.abos.mc.strangeadventures.item.ModItems;
 
-public class SapSipperBlockEntity extends BlockEntity {
+public class SapSipperBlockEntity extends BlockEntity implements BalmFluidTankProvider {
+
+    public static final int MAX_CAPACITY = 1000; // in mB
+
+    protected int extractTicksRemaining;
+    protected Tank tank = new Tank();
+
+    protected boolean isDirty;
 
     public SapSipperBlockEntity(final BlockPos pos, final BlockState state) {
         super(ModBlockEntities.SAP_SIPPER.value(), pos, state);
     }
 
-    public static void tick(final Level level, final BlockPos pos, final BlockState state, final SapSipperBlockEntity sapSipper) {
+    @Override
+    public FluidTank getFluidTank() {
+        return tank;
+    }
+
+    @Override
+    protected void saveAdditional(final ValueOutput output) {
+        output.putInt("ExtractTicksRemaining", extractTicksRemaining);
+        tank.serialize(output.child("Tank"));
+        super.saveAdditional(output);
+    }
+
+    @Override
+    protected void loadAdditional(final ValueInput input) {
+        super.loadAdditional(input);
+        input.child("Tank").ifPresent(tank::deserialize);
+        extractTicksRemaining = input.getIntOr("ExtractTicksRemaining", 0);
+    }
+
+    public static void tick(final Level level, final BlockPos pos, final BlockState state, final SapSipperBlockEntity entity) {
         final BlockState blockBehind = level.getBlockState(pos.relative(state.getValue(HorizontalDirectionalBlock.FACING).getOpposite(), 1));
+        ((BucketItem)ModItems.BIRCH_SAP_BUCKET.asItem()).getContent();
         //StrangeAdventures.logger.info("Facing: " + blockBehind.getBlock().getDescriptionId());
+    }
+
+    public class Tank extends DefaultFluidTank {
+
+        public Tank() {
+            super(MAX_CAPACITY);
+        }
+
+        @Override
+        public void setChanged() {
+            SapSipperBlockEntity.this.setChanged();
+            isDirty = true;
+        }
     }
 
 }
