0f6bd3e3b2fdfcaa42d572f9c9f99fab86c37276
Ayutac <fly.high.android@gmail.com>
2026-08-22 23:57:31 +0200

    Added Makes-X-Sap tags.

--- common/src/main/java/studio/abos/mc/strangeadventures/tag/ModItemTags.java
+++ common/src/main/java/studio/abos/mc/strangeadventures/tag/ModItemTags.java
@@ -6,5 +6,17 @@ import net.minecraft.world.item.Item;
 import studio.abos.mc.strangeadventures.StrangeAdventures;
 
 public final class ModItemTags {
-    public static final TagKey<Item> YOUR_TAG = TagKey.create(Registries.ITEM, StrangeAdventures.id("your_tag"));
+
+    public static final TagKey<Item> MAKES_ACACIA_SAP = TagKey.create(Registries.ITEM, StrangeAdventures.id("makes_acadia_sap"));
+    public static final TagKey<Item> MAKES_BIRCH_SAP = TagKey.create(Registries.ITEM, StrangeAdventures.id("makes_birch_sap"));
+    public static final TagKey<Item> MAKES_CACTUS_SAP = TagKey.create(Registries.ITEM, StrangeAdventures.id("makes_cactus_sap"));
+    public static final TagKey<Item> MAKES_CHERRY_SAP = TagKey.create(Registries.ITEM, StrangeAdventures.id("makes_cherry_sap"));
+    public static final TagKey<Item> MAKES_CHORUS_SAP = TagKey.create(Registries.ITEM, StrangeAdventures.id("makes_chorus_sap"));
+    public static final TagKey<Item> MAKES_CRIMSON_SAP = TagKey.create(Registries.ITEM, StrangeAdventures.id("makes_crimson_sap"));
+    public static final TagKey<Item> MAKES_JUNGLE_SAP = TagKey.create(Registries.ITEM, StrangeAdventures.id("makes_jungle_sap"));
+    public static final TagKey<Item> MAKES_MANGROVE_SAP = TagKey.create(Registries.ITEM, StrangeAdventures.id("makes_mangrove_sap"));
+    public static final TagKey<Item> MAKES_OAK_SAP = TagKey.create(Registries.ITEM, StrangeAdventures.id("makes_oak_sap"));
+    public static final TagKey<Item> MAKES_SPRUCE_SAP = TagKey.create(Registries.ITEM, StrangeAdventures.id("makes_spruce_sap"));
+    public static final TagKey<Item> MAKES_WARPED_SAP = TagKey.create(Registries.ITEM, StrangeAdventures.id("makes_warped_sap"));
+
 }
