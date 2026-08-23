f0f8ad27c7117583c39dd61fcd2a68a5d48f5852
Ayutac <fly.high.android@gmail.com>
2026-08-23 08:46:50 +0200

    Added missing translations.

--- common/src/main/java/studio/abos/mc/strangeadventures/fluid/ModFluidIds.java
+++ common/src/main/java/studio/abos/mc/strangeadventures/fluid/ModFluidIds.java
@@ -1,6 +1,5 @@
 package studio.abos.mc.strangeadventures.fluid;
 
-import lombok.NonNull;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.material.Fluid;
@@ -8,27 +7,27 @@ import studio.abos.mc.strangeadventures.StrangeAdventures;
 
 public final class ModFluidIds {
 
-    public static final ResourceKey<@NonNull Fluid> ACACIA_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("acacia_sap"));
-    public static final ResourceKey<@NonNull Fluid> ACACIA_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_acacia_sap"));
-    public static final ResourceKey<@NonNull Fluid> BIRCH_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("birch_sap"));
-    public static final ResourceKey<@NonNull Fluid> BIRCH_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_birch_sap"));
-    public static final ResourceKey<@NonNull Fluid> CACTUS_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("cactus_sap"));
-    public static final ResourceKey<@NonNull Fluid> CACTUS_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_cactus_sap"));
-    public static final ResourceKey<@NonNull Fluid> CHERRY_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("cherry_sap"));
-    public static final ResourceKey<@NonNull Fluid> CHERRY_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_cherry_sap"));
-    public static final ResourceKey<@NonNull Fluid> CHORUS_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("chorus_sap"));
-    public static final ResourceKey<@NonNull Fluid> CHORUS_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_chorus_sap"));
-    public static final ResourceKey<@NonNull Fluid> CRIMSON_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("crimson_sap"));
-    public static final ResourceKey<@NonNull Fluid> CRIMSON_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_crimson_sap"));
-    public static final ResourceKey<@NonNull Fluid> JUNGLE_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("jungle_sap"));
-    public static final ResourceKey<@NonNull Fluid> JUNGLE_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_jungle_sap"));
-    public static final ResourceKey<@NonNull Fluid> MANGROVE_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("mangrove_sap"));
-    public static final ResourceKey<@NonNull Fluid> MANGROVE_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_mangrove_sap"));
-    public static final ResourceKey<@NonNull Fluid> OAK_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("oak_sap"));
-    public static final ResourceKey<@NonNull Fluid> OAK_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_oak_sap"));
-    public static final ResourceKey<@NonNull Fluid> SPRUCE_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("spruce_sap"));
-    public static final ResourceKey<@NonNull Fluid> SPRUCE_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_spruce_sap"));
-    public static final ResourceKey<@NonNull Fluid> WARPED_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("warped_sap"));
-    public static final ResourceKey<@NonNull Fluid> WARPED_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_warped_sap"));
+    public static final ResourceKey<Fluid> ACACIA_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("acacia_sap"));
+    public static final ResourceKey<Fluid> ACACIA_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_acacia_sap"));
+    public static final ResourceKey<Fluid> BIRCH_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("birch_sap"));
+    public static final ResourceKey<Fluid> BIRCH_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_birch_sap"));
+    public static final ResourceKey<Fluid> CACTUS_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("cactus_sap"));
+    public static final ResourceKey<Fluid> CACTUS_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_cactus_sap"));
+    public static final ResourceKey<Fluid> CHERRY_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("cherry_sap"));
+    public static final ResourceKey<Fluid> CHERRY_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_cherry_sap"));
+    public static final ResourceKey<Fluid> CHORUS_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("chorus_sap"));
+    public static final ResourceKey<Fluid> CHORUS_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_chorus_sap"));
+    public static final ResourceKey<Fluid> CRIMSON_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("crimson_sap"));
+    public static final ResourceKey<Fluid> CRIMSON_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_crimson_sap"));
+    public static final ResourceKey<Fluid> JUNGLE_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("jungle_sap"));
+    public static final ResourceKey<Fluid> JUNGLE_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_jungle_sap"));
+    public static final ResourceKey<Fluid> MANGROVE_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("mangrove_sap"));
+    public static final ResourceKey<Fluid> MANGROVE_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_mangrove_sap"));
+    public static final ResourceKey<Fluid> OAK_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("oak_sap"));
+    public static final ResourceKey<Fluid> OAK_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_oak_sap"));
+    public static final ResourceKey<Fluid> SPRUCE_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("spruce_sap"));
+    public static final ResourceKey<Fluid> SPRUCE_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_spruce_sap"));
+    public static final ResourceKey<Fluid> WARPED_SAP_STILL = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("warped_sap"));
+    public static final ResourceKey<Fluid> WARPED_SAP_FLOWING = ResourceKey.create(Registries.FLUID, StrangeAdventures.id("flowing_warped_sap"));
 
 }
