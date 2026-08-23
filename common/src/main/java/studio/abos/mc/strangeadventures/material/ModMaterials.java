3b2329ea370cbfc3c57b825380ac335b60f2bb3d
Ayutac <fly.high.android@gmail.com>
2026-08-23 22:17:58 +0200

    Prepared ModMaterials.

---
+++ common/src/main/java/studio/abos/mc/strangeadventures/material/ModMaterials.java
@@ -0,0 +1,7 @@
+package studio.abos.mc.strangeadventures.material;
+
+public final class ModMaterials {
+
+
+
+}
---
+++ common/src/main/java/studio/abos/mc/strangeadventures/material/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package studio.abos.mc.strangeadventures.material;
+
+import org.jspecify.annotations.NullMarked;
