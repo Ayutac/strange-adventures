4eae6411feb386b95d81e6a7006610643c9bbabe
Ayutac <fly.high.android@gmail.com>
2026-08-24 15:01:28 +0200

    Some missing translations and fixed the weir sapling item display.

--- fabric/src/main/java/studio/abos/mc/strangeadventures/fabric/datagen/ModModelProvider.java
+++ fabric/src/main/java/studio/abos/mc/strangeadventures/fabric/datagen/ModModelProvider.java
@@ -21,8 +21,9 @@ public class ModModelProvider extends FabricModelProvider {
     @Override
     public void generateBlockStateModels(final BlockModelGenerators blockStateModelGenerator) {
         blockStateModelGenerator.createPlant(ModBlocks.GREEN_FLOWER.asBlock(), ModBlocks.POTTED_GREEN_FLOWER.asBlock(), BlockModelGenerators.PlantType.NOT_TINTED);
-        blockStateModelGenerator.createPlant(ModBlocks.WEIR_SAPLING.asBlock(), ModBlocks.POTTED_WEIR_SAPLING.asBlock(), BlockModelGenerators.PlantType.NOT_TINTED);
         blockStateModelGenerator.registerSimpleFlatItemModel(ModBlocks.GREEN_FLOWER.asBlock());
+        blockStateModelGenerator.createPlant(ModBlocks.WEIR_SAPLING.asBlock(), ModBlocks.POTTED_WEIR_SAPLING.asBlock(), BlockModelGenerators.PlantType.NOT_TINTED);
+        blockStateModelGenerator.registerSimpleFlatItemModel(ModBlocks.WEIR_SAPLING.asBlock());
         createGreenFarmland(blockStateModelGenerator);
         blockStateModelGenerator.createNonTemplateModelBlock(ModBlocks.GREEN_CACTUS.asBlock());
         blockStateModelGenerator.family(ModBlocks.WEIR_PLANKS.asBlock()).generateFor(ModBlockFamilies.WEIR_PLANKS);
