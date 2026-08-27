e3cdd5b9a0aeeb3aedc6eb4daa960a00fdcfdf45
Ayutac <fly.high.android@gmail.com>
2026-08-27 12:42:06 +0200

    Added Green Cactus textures.

--- common/src/main/java/studio/abos/mc/strangeadventures/block/GreenCactusBlock.java
+++ common/src/main/java/studio/abos/mc/strangeadventures/block/GreenCactusBlock.java
@@ -4,11 +4,18 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.FluidTags;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CactusBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class GreenCactusBlock extends CactusBlock {
+
+    protected static final VoxelShape SHAPE_COLLISION = Block.column(8d, 0d, 15d);
+    protected static final VoxelShape SHAPE = Block.column(8d, 0d, 16d);
 
     protected GreenCactusBlock(final Properties properties) {
         super(properties);
@@ -23,6 +30,15 @@ public class GreenCactusBlock extends CactusBlock {
         }
         BlockState belowState = level.getBlockState(pos.below());
         return (belowState.is(this) || belowState.is(BlockTags.SUPPORTS_CACTUS)) && !level.getBlockState(pos.above()).liquid();
+
+    }
+
+    protected VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE_COLLISION;
+    }
+
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
     }
 
 }
