f47c26a48df143dedc1d73be8a8e4596c058f4ba
Ayutac <fly.high.android@gmail.com>
2026-09-12 12:31:12 +0200

    Fixed JEI on NeoForged.

--- neoforge/src/main/java/studio/abos/mc/strangeadventures/neoforge/NeoForgeStrangeAdventures.java
+++ neoforge/src/main/java/studio/abos/mc/strangeadventures/neoforge/NeoForgeStrangeAdventures.java
@@ -5,13 +5,22 @@ import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.fluids.FluidType;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import studio.abos.mc.strangeadventures.StrangeAdventures;
 
 @Mod(StrangeAdventures.MOD_ID)
 public final class NeoForgeStrangeAdventures {
 
+    public static final DeferredRegister<FluidType> FLUID_TYPES = DeferredRegister.create(NeoForgeRegistries.FLUID_TYPES, StrangeAdventures.MOD_ID);
+
+    public static final DeferredHolder<FluidType, FluidType> SAP_FLUID_TYPE = FLUID_TYPES.register("sap_fluid", () -> new FluidType(FluidType.Properties.create()));
+
     public NeoForgeStrangeAdventures(final ModContainer modContainer, final IEventBus modEventBus) {
         final var context = new NeoForgeLoadContext(modContainer, modEventBus);
+        FLUID_TYPES.register(modEventBus);
         Balm.initializeMod(StrangeAdventures.MOD_ID, context, StrangeAdventures::initialize);
     }
 
--- neoforge/src/main/java/studio/abos/mc/strangeadventures/neoforge/mixin/AbstractSapFluidMixin.java
+++ neoforge/src/main/java/studio/abos/mc/strangeadventures/neoforge/mixin/AbstractSapFluidMixin.java
@@ -3,18 +3,15 @@ package studio.abos.mc.strangeadventures.neoforge.mixin;
 import net.neoforged.neoforge.common.extensions.IFluidExtension;
 import net.neoforged.neoforge.fluids.FluidType;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import studio.abos.mc.strangeadventures.fluid.AbstractSapFluid;
+import studio.abos.mc.strangeadventures.neoforge.NeoForgeStrangeAdventures;
 
 @Mixin(AbstractSapFluid.class)
 public abstract class AbstractSapFluidMixin implements IFluidExtension {
 
-    @Unique
-    private static final FluidType SAP_FLUID_TYPE = new FluidType(FluidType.Properties.create());
-
     @Override
     public FluidType getFluidType() {
-        return SAP_FLUID_TYPE;
+        return NeoForgeStrangeAdventures.SAP_FLUID_TYPE.value();
     }
 
 }
