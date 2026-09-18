9b77402cb98986df5f013c2cf9ff323df94c0d1a
Ayutac <fly.high.android@gmail.com>
2026-09-18 16:27:44 +0200

    Added increasing mass by eating leaves.

--- common/src/main/java/studio/abos/mc/strangeadventures/mixin/BlockBehaviorMixin.java
+++ common/src/main/java/studio/abos/mc/strangeadventures/mixin/BlockBehaviorMixin.java
@@ -2,10 +2,16 @@ package studio.abos.mc.strangeadventures.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.BlockItemTags;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.EntityCollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -33,6 +39,19 @@ public abstract class BlockBehaviorMixin {
                     }
                 }
             }
+        }
+        return original;
+    }
+
+    @ModifyReturnValue(method = "useWithoutItem(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/phys/BlockHitResult;)Lnet/minecraft/world/InteractionResult;", at = @At("RETURN"))
+    protected InteractionResult strangeadventures$eatCompostable(final InteractionResult original, final BlockState state, final Level level, final BlockPos pos, final Player player, final BlockHitResult hitResult) {
+        if (original == InteractionResult.PASS && StrangeAdventuresApi.INTERNAL_METHODS.greenAvatarActive(player) && state.is(BlockItemTags.LEAVES.block())) {
+            if (level.isClientSide()) {
+                return InteractionResult.SUCCESS;
+            }
+            level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
+            StrangeAdventuresApi.INTERNAL_METHODS.greenAvatarAddMass((ServerPlayer)player, 0.3f * (float)(Math.max(1e-4, Math.pow((Math.cos(Math.PI * StrangeAdventuresApi.INTERNAL_METHODS.greenAvatarGetMass(player) / StrangeAdventuresApi.GREEN_AVATAR_MASS_MAX) + 1) / 2, 4))));
+            return InteractionResult.SUCCESS;
         }
         return original;
     }
